c923fc5ee040a12ffa9d1a3b78b59afb7e6521e1
Paulo Becker <beckerpaulo81@gmail.com>
2022-06-07 10:09:57 -0300

    Criado os layouts de listagem das partidas

--- app/src/main/java/me/dio/simulator/DetailActivity.kt
+++ app/src/main/java/me/dio/simulator/DetailActivity.kt
@@ -3,9 +3,9 @@ package me.dio.simulator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DetailActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detail2)
     }
 }
